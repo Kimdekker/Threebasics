a49ccbe56d47c5801156a41aeb3bd3dbf728f430
Kim Dekker <kim11dekker@gmail.com>
2025-02-11 20:57:11 +0100

    Boilerplate ready

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import * as THREE from 'three';
 
-// Create scene
 const scene = new THREE.Scene();
-
-// Create camera
-const camera = new THREE.PerspectiveCamera(  // PerspectiveCamera(fov, aspect, near, far)
-    75, // field of view
-    window.innerWidth / window.innerHeight, // aspect ratio
-);
-camera.position.z = 3;
 
 // Create renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+
+
+
+
+
+
 
 
 
